2ac4480905f92455aa948030b9e8ee91265fa318
NickyGuants <nicholasguantai528@gmail.com>
2021-10-26 19:03:56 +0300

    create navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../App.css';
+
+const Navigation = () =>{
+    return(
+        <div>
+            <header>
+                <nav>
+                    <a href="#home">HOME</a>
+                    <a href="#products">PRODUCTS</a>
+                    <a href="#search">SEARCH</a>
+                    <a href="#cart">CART</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Navigation;
