63d9eeed1a41cbb024c75708411dd3d795b3b2fa
NickyGuants <nicholasguantai528@gmail.com>
2021-10-27 15:42:37 +0300

    add vehicle data

---
+++ src/components/products/vehicles.js
@@ -0,0 +1,94 @@
+export const vehicles = [
+    {
+      id: 0,
+      name: "AUDI A3 CONVERTIBLE 2015",
+      price: 1000,
+      make: "Audi",
+      model: "A3",
+      year: "2015",
+      instock: 43,
+      description:
+      "AUDI A3 CONVERTIBLE 2015",
+      imgSrc: "https://images.unsplash.com/photo-1589651756400-3cc85d74f743?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+      fuel: "",
+      transmission: ["Manual", "Automatic"],
+      engineSize: ""
+    },
+    {
+      id: 1,
+      name: "Porshe",
+      price: 1000,
+      make: "",
+      model: "",
+      year: "",
+      instock: 43,
+      description:
+      "Porshe",
+      imgSrc: "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      fuel: "",
+      transmission: ["Manual", "Automatic"],
+      engineSize: ""
+      
+    },
+    {
+      id: 2,
+      name: "Porshe",
+      price: 1000,
+      make: "",
+      model: "",
+      year: "",
+      instock: 43,
+      description:
+      "Porshe",
+      imgSrc: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+      fuel: "",
+      transmission: ["Manual", "Automatic"],
+      engineSize: ""
+    },
+    {
+        id: 3,
+        name: "Porshe",
+        price: 1000,
+        make: "",
+        model: "",
+        year: "",
+        instock: 43,
+        description:
+        "Porshe",
+        imgSrc: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+        fuel: "",
+        transmission: ["Manual", "Automatic"],
+        engineSize: ""
+      },
+      {
+        id: 4,
+        name: "Porshe",
+        price: 1000,
+        make: "",
+        model: "",
+        year: "",
+        instock: 43,
+        description:
+        "Porshe",
+        imgSrc: "https://images.unsplash.com/photo-1593142927592-71ed12446cd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+        fuel: "",
+        transmission: ["Manual", "Automatic"],
+        engineSize: ""
+      },
+      {
+        id: 5,
+        name: "Porshe",
+        price: 1000,
+        make: "",
+        model: "",
+        year: "",
+        instock: 43,
+        description:
+        "Porshe",
+        imgSrc: "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+        fuel: "",
+        transmission: ["Manual", "Automatic"],
+        engineSize: ""
+      }
+
+  ];
