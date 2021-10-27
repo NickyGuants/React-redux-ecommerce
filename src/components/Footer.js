bdfc3a03fdff21e6bd311481bc7f3d696bb0132b
NickyGuants <nicholasguantai528@gmail.com>
2021-10-27 15:41:15 +0300

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer =() =>{
+    return (
+            <div className="footer">
+            <footer>
+                <p>Copyright &copy; Pearl Shine Motors, 2021</p>
+            </footer>
+            </div>
+    )
+
+}
+
+export default Footer;
