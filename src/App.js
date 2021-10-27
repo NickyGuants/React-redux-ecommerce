1d91d1f9384bc2bfb31e1e37714ba428eab43623
NickyGuants <nicholasguantai528@gmail.com>
2021-10-27 07:44:05 +0300

    created the hero slider section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Hero from './components/hero/Hero';
 import Navigation from './components/Navigation';
+import { SliderData } from './components/hero/SliderData';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Hero slides={SliderData} />
       
     </div>
   );
---
+++ src/components/hero/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+      image:
+        'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1579272154060-4e7a4d0f5033?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=940&q=80'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1592032857148-5658283bb67b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjV8fHZlaGljbGVzJTIwaW4lMjBzaG93cm9vbXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1626847037657-fd3622613ce3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1592797520856-883837ddd186?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    }
+  ];
