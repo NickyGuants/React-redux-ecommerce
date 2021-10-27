82e87d3b228dfeec9585278872ad72a211f513f9
NickyGuants <nicholasguantai528@gmail.com>
2021-10-27 15:41:03 +0300

    add product and footer sections

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Hero from './components/hero/Hero';
 import Navigation from './components/Navigation';
 import { SliderData } from './components/hero/SliderData';
+import Product from './components/products/Products';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Hero slides={SliderData} />
-      
+      <Product />
+      <Footer/>
     </div>
   );
 }
