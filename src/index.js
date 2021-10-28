8c4bdae1509dd41dac4e0ba81bf261fdeda0a9c9
NickyGuants <nicholasguantai528@gmail.com>
2021-10-28 15:31:50 +0300

    add store to app using provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react';
 import './index.css';
 import App from './App';
+import store from './store'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
