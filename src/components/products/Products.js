3f0de2f66f8731f3b75b56dea49871d4e71d5bc1
NickyGuants <nicholasguantai528@gmail.com>
2021-10-29 15:33:11 +0300

    changes in rendering products

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { AiOutlineShoppingCart} from 'react-icons/ai';
-import { vehicles } from "./vehicles";
+//import { vehicles } from "./vehicles";
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem } from '/home/nicky/react-ecommerce/src/redux/actions/cartActions'
+import { Link } from 'react-router-dom'
 
-const Product =() =>{
+const Product = () => {
+    const  products  = useSelector((state) => state.cart.vehicles);
+    const dispatch = useDispatch();
+
     return (
         <section className="container">
             <div className="products">
-                {vehicles.map((vehicle) =>{
+                {products.map((vehicle) =>{
                     return(
-                        <div className="card">
+                        <div className="card" key={vehicle.id}>
+                            <Link to={`/details/${vehicle.id}`}>
                             <img src={vehicle.imgSrc} alt={vehicle.name} />
+                            </Link>
                             <p>{vehicle.name}</p>
                             <p>Price: ${vehicle.price}</p>
                             <p>{vehicle.description}</p>
                             <div class="add-to-cart" >
-                                <button><AiOutlineShoppingCart className="cart-icon" />Add to Cart </button>
+                                <button onClick={()=>dispatch(addItem(vehicle.id))}><AiOutlineShoppingCart className="cart-icon" />Add to Cart </button>
                             </div>
                             <div className="units">
                                 <div class="minus">-</div>
