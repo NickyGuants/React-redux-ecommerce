b847603852159fb2de83d69500c63ad8aa87c125
NickyGuants <nicholasguantai528@gmail.com>
2021-10-29 18:24:40 +0300

    fix the add item reducer

--- src/redux/reducers/cartReducers.js
+++ src/redux/reducers/cartReducers.js
@@ -3,21 +3,19 @@ import { vehicles } from "../../components/products/vehicles";
 
 const initialState = {
     cartItems: [],
-    vehicles
+    vehicles: [...vehicles]
 }
 
 const cartReducer = (state = initialState, { payload, type }) => {
     switch (type) {
         case ADD_ITEM:
             const item = state.vehicles.find((vehicle) =>
-                vehicle.id === payload.id);
-            
-            return [
+                vehicle.id === payload);
+            console.log(item);
+            return {
                 ...state,
-                state.cartItems.push({
-                    ...item,
-                })
-            ]
+                cartItems: [...state.cartItems, item]
+            }
         case REMOVE_ITEM:
             return state.filter((item) => item.id !== payload);
         default: return state;
