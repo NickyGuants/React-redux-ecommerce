543156bea180c4c98d4323d4b12d2db86ea251c4
NickyGuants <nicholasguantai528@gmail.com>
2021-10-29 15:30:27 +0300

    add client routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
-import Hero from './components/hero/Hero';
 import Navigation from './components/Navigation';
-import { SliderData } from './components/hero/SliderData';
-import Product from './components/products/Products';
 import Footer from './components/Footer';
 import Cart from './components/Cart';
+import ProductDetails from './components/ProductDetails';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Hero slides={SliderData} />
-      <Product />
-      <Cart />
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+          <Switch>
+            <Route path="/"  exact><Home /></Route>
+            <Route path="/cart" ><Cart /></Route>
+            <Route path="/details" ><ProductDetails /></Route>
+          </Switch>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
