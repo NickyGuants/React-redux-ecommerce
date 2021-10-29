9d08fa4a18ec7a9d47f1a36ce4fc452054dfdebf
NickyGuants <nicholasguantai528@gmail.com>
2021-10-29 15:32:53 +0300

    create product details page

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductDetails = () => {
+    return (
+        <section>
+            <div>
+                <h1>Details</h1>
+            </div>
+        </section>
+    )
+}
+
+export default ProductDetails;
