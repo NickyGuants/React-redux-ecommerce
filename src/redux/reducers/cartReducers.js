bc1f1d12fecaa0f291269692ef89ea64ef1c9b73
NickyGuants <nicholasguantai528@gmail.com>
2021-11-02 09:42:56 +0300

    add reducer for incrementing and decrementing

--- src/redux/reducers/cartReducers.js
+++ src/redux/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { ADD_ITEM, REMOVE_ITEM } from '../types'
+import { ADD_ITEM, REMOVE_ITEM, INCREMENT_ITEM, DECREMENT_ITEM } from '../types'
 import { vehicles } from "../../components/products/vehicles";
 
 const initialState = {
@@ -9,15 +9,39 @@ const initialState = {
 const cartReducer = (state = initialState, { payload, type }) => {
     switch (type) {
         case ADD_ITEM:
-            const item = state.vehicles.find((vehicle) =>
-                vehicle.id === payload);
+            //get the item from the vehicle array
+            const item = state.vehicles.find((vehicle) => vehicle.id === payload);
+            //check whether the item exists in the cart
+            const inCart = state.cartItems.find((item) => item.id === payload ? true : false);
             console.log(item);
             return {
                 ...state,
-                cartItems: [...state.cartItems, item]
+                cartItems: inCart ?
+                state.cartItems.map((item) => item.id === payload
+                  ? { ...item, quantity: item.quantity + 1 } : item )
+                : [...state.cartItems, { ...item, quantity: 1 }]
             }
         case REMOVE_ITEM:
-            return state.filter((item) => item.id !== payload);
+            const fileteredCart = state.cartItems.filter((item) => item.id !== payload);
+            return { ...state, cartItems: [...fileteredCart] }
+        case INCREMENT_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.map((item) =>
+                  item.id === payload
+                    ? { ...item, quantity: item.quantity+1 }
+                    : item
+                )
+            }
+        case DECREMENT_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.map((item) =>
+                  item.id === payload
+                    ? { ...item, quantity: item.quantity-1 }
+                    : item
+                )
+            }
         default: return state;
     }
 }
