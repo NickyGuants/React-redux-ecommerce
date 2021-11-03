fc0b4bc5b81e2ef18878448ef9c8820ee070d9c1
NickyGuants <nicholasguantai528@gmail.com>
2021-11-03 07:27:32 +0300

    create search car type

--- src/redux/types.js
+++ src/redux/types.js
@@ -2,3 +2,4 @@ export const ADD_ITEM=  "ADD_ITEM";
 export const REMOVE_ITEM = "REMOVE_ITEM";
 export const INCREMENT_ITEM = "INCREMENT_ITEM";
 export const DECREMENT_ITEM = "DECREMENT_ITEM";
+export const SEARCH_CARS ="SEARCH_CARS "
