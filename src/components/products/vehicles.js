a26c1b28ca0fda7107797e74648b179c14091957
NickyGuants <nicholasguantai528@gmail.com>
2021-11-03 10:53:20 +0300

    add missing details

--- src/components/products/vehicles.js
+++ src/components/products/vehicles.js
@@ -33,7 +33,7 @@ export const vehicles = [
     {
       id: 2,
       name: "Chelby Mustang",
-      price: 180000,
+      price: 280000,
       make: "Ford",
       model: "Mustang",
       year: "2021",
@@ -41,54 +41,54 @@ export const vehicles = [
       description:
       "Chelby Mustang",
       imgSrc: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-      fuel: "",
+      fuelCapacity: "50L",
       transmission: "Automatic",
-      engineSize: ""
+      engineSize: "3000cc"
     },
     {
         id: 3,
         name: "Porshe",
-        price: 1000,
-        make: "",
-        model: "",
-        year: "",
+        price: 450000,
+        make: "Porshe",
+        model: "Porshe",
+        year: "2020",
         instock: 43,
         description:
         "Porshe",
         imgSrc: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-        fuel: "",
+        fuelCapacity: "40L",
         transmission: "Automatic",
-        engineSize: ""
+        engineSize: "2500cc"
       },
       {
         id: 4,
-        name: "Porshe",
-        price: 1000,
-        make: "",
-        model: "",
-        year: "",
+        name: "Mustang",
+        price: 320000,
+        make: "M32",
+        model: "Mustang",
+        year: "2017",
         instock: 43,
         description:
         "Porshe",
         imgSrc: "https://images.unsplash.com/photo-1593142927592-71ed12446cd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-        fuel: "",
+        fuelCapacity: "35L",
         transmission: "Automatic",
-        engineSize: ""
+        engineSize: "5000cc"
       },
       {
         id: 5,
-        name: "Porshe",
-        price: 1000,
-        make: "",
-        model: "",
-        year: "",
+        name: "Mercedez",
+        price: 200000,
+        make: "M2",
+        model: "Mercedex",
+        year: "2018",
         instock: 43,
         description:
         "Porshe",
         imgSrc: "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-        fuel: "",
+        fuelCapacity: "30L",
         transmission: "Automatic",
-        engineSize: ""
+        engineSize: "4000cc"
       }
 
   ];
