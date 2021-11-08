8432db21d4f65901ce9172f3fd2073dc4af026bf
NickyGuants <nicholasguantai528@gmail.com>
2021-11-08 16:17:05 +0300

    add cart icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,12 @@ import React from "react";
 import '../App.css';
 import { Link } from 'react-router-dom'
 import Search from "./Search";
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useSelector } from 'react-redux';
 
 
-const Navigation = () =>{
+const Navigation = () => {
+    const  cartItems  = useSelector((state) => state.cart.cartItems)
     return(
         <div>
             <header>
@@ -13,8 +16,8 @@ const Navigation = () =>{
                 </div>
                 <Search />
                 <nav>  
-                    <Link to="/">HOME</Link>
-                    <Link to="/cart">CART</Link>
+                    <Link to="/">HOME</Link> 
+                    <Link to="/cart" className="cart-link"><AiOutlineShoppingCart id="cart-icon" /><span>{ cartItems.length}</span></Link>
                 </nav>
             </header>
         </div>
